884511afba23e9f272dab3758787068d8b19dd65
Jonathan Watson <thejmdw@gmail.com>
2021-04-19 14:40:42 -0400

    finished sowing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,43 +1,56 @@
 import {createPlan} from "./plan.js"
-import {createAsparagus} from "./seeds/asparagus.js"
-import {createCorn} from "./seeds/corn.js"
-import {createPotato} from "./seeds/potato.js"
-import {createSoybean} from "./seeds/soybean.js"
-import {createSunflower} from "./seeds/sunflower.js"
-import {createWheat} from "./seeds/wheat.js"
+// import {createAsparagus} from "./seeds/asparagus.js"
+// import {createCorn} from "./seeds/corn.js"
+// import {createPotato} from "./seeds/potato.js"
+// import {createSoybean} from "./seeds/soybean.js"
+// import {createSunflower} from "./seeds/sunflower.js"
+// import {createWheat} from "./seeds/wheat.js"
 import {addPlant, usePlants} from "./field.js"
+import { plantSeeds } from "./tractor.js"
 
-//Harvest Plan
+
+// //Harvest Plan
 const yearlyPlan = createPlan()
 
-console.log(yearlyPlan)
+// console.log(yearlyPlan)
+// console.log("-----")
+
+// //Creating Seeds
+// const asparagusSeed = createAsparagus()
+// const cornSeed = createCorn()
+// const potatoSeed = createPotato()
+// const soybeanSeed = createSoybean()
+// const sunflowerSeed = createSunflower()
+// const wheatSeed = createWheat()
+
+
+// console.log(asparagusSeed)
+// console.log(cornSeed)
+// console.log(potatoSeed)
+// console.log(soybeanSeed)
+// console.log(sunflowerSeed)
+// console.log(wheatSeed)
+// console.log("-----")
+
+// Tilling the Field
+// addPlant(asparagusSeed)
+// addPlant(cornSeed)
+// addPlant(potatoSeed)
+// addPlant(soybeanSeed)
+// addPlant(sunflowerSeed)
+// addPlant(wheatSeed)
+
+// const plantsUsed = usePlants()
+
+// console.log(plantsUsed)
 console.log("-----")
-//Creating Seeds
-const asparagusSeed = createAsparagus()
-const cornSeed = createCorn()
-const potatoSeed = createPotato()
-const soybeanSeed = createSoybean()
-const sunflowerSeed = createSunflower()
-const wheatSeed = createWheat()
 
-
-console.log(asparagusSeed)
-console.log(cornSeed)
-console.log(potatoSeed)
-console.log(soybeanSeed)
-console.log(sunflowerSeed)
-console.log(wheatSeed)
+//Sowing with the Tractor
 console.log("-----")
-//Tilling the Field
-addPlant(asparagusSeed)
-addPlant(cornSeed)
-addPlant(potatoSeed)
-addPlant(soybeanSeed)
-addPlant(sunflowerSeed)
-addPlant(wheatSeed)
-
+plantSeeds(yearlyPlan)
 const plantsUsed = usePlants()
 
 console.log(plantsUsed)
-console.log("-----")
 
+console.log("-----")
+//Harvesting
